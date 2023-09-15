aacf101c5ad429fad13e0c97dfb7c2523c6837ab
Auca Maillot <auca.maillot@farmacity.com.ar>
2023-09-15 18:59:19 -0300

    agregue react

---
+++ types-clone-react/src/App.js
@@ -0,0 +1,111 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <body>
+        <div class="head_links">
+          <header class="nav_links">
+            <nav>
+
+              <a class="head_enlace" href="file:///C:/Users/Usuario/Desktop/L%C3%B3gica%20de%20Programaci%C3%B3n/P%C3%A1gina09/typefaces.html">Typefaces</a>
+              <a class="head_enlace" href="file:///C:/Users/Usuario/Desktop/L%C3%B3gica%20de%20Programaci%C3%B3n/P%C3%A1gina09/custom.html">Custom</a>
+              <a class="head_enlace" href="file:///C:/Users/Usuario/Desktop/L%C3%B3gica%20de%20Programaci%C3%B3n/P%C3%A1gina09/studio.html">Studio</a>
+              <a class="head_enlace" href="file:///C:/Users/Usuario/Desktop/L%C3%B3gica%20de%20Programaci%C3%B3n/P%C3%A1gina09/cart.html">Cart</a>
+
+
+
+
+            </nav>
+
+
+
+
+          </header>
+        </div>
+        <main>
+          <div class="container">
+
+            <section class="typefaces_header">
+              <h1 class="types_header"> Items • 3 families • 39 styles </h1>
+              <a class="item" href="#">Item</a>
+            </section>
+            <section class="typefaces_header">
+              <h1 class="types_header">Lateral • 7 families • 98 styles </h1>
+              <a class="lateral" href="#">Lateral</a>
+            </section>
+            <section class="typefaces_header">
+              <h1 class="types_header"> Apris • 1 family • 12 styles </h1>
+              <a class="apris" href="#">Apris</a>
+            </section>
+
+
+
+
+            <footer class="footer_container">
+              <div class="footer">
+                <div class="typefaces_footer">
+                  <div class="typefaces_fonts">
+                    <h4>Typefaces</h4>
+                    <ul class="list_items_ul">
+                      <li><a class="list_items" href="">Items</a></li>
+                      <li><a class="list_items" href="">Lateral</a></li>
+                      <li><a class="list_items" href="">Apris</a></li>
+                      <li><a class="list_items" href="">Scto Grotesk</a></li>
+                      <li><a class="list_items" href="">Noe</a></li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="custom_footer">
+                  <div class="custom_items">
+                    <h4>Custom</h4>
+                    <ul class="list_items_ul">
+                      <li><a class="list_items" href="">Kinfolk</a></li>
+                      <li><a class="list_items" href="">Marimekko</a></li>
+                      <li><a class="list_items" href="">Moda Operandi</a></li>
+                      <li><a class="list_items" href="">Söderströms</a></li>
+                      <li><a class="list_items" href="">Demi</a></li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="info_footer">
+                  <div class="info_items">
+                    <h4>Information</h4>
+                    <ul class="list_items_ul">
+                      <li><a class="list_items" href="">Studio</a></li>
+                      <li><a class="list_items" href="">Licenses</a></li>
+                      <li><a class="list_items" href="">Disclaimer</a></li>
+                      <li><a class="list_items" href="">Contact</a></li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="contact_footer">
+                  <div class="contact__items">
+                    <h4>Get in touch</h4>
+                    <ul class="list_items_ul">
+                      <li><a class="list_items" href="">Email</a></li>
+                      <li><a class="list_items" href="">Facebook</a></li>
+                      <li><a class="list_items" href="">Instagram</a></li>
+                      <li><a class="list_items" href="">Twitter</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+          
+        </footer>
+        </div>
+
+      </main>
+    </body >
+    </>
+
+
+  );
+}
+
+export default App;
