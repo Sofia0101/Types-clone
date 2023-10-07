56cae656942c694e1fa764c50e2a28b8c67b957d
Sofía Fornaroli <Sofia0101>
2023-10-07 14:29:41 -0300

    agrego componente footer.jsx

---
+++ types-clone-react/src/components/footer.jsx
@@ -0,0 +1,63 @@
+function MiFooter(props) {
+    return (
+
+        <footer className="footer_container">
+            <div className="footer">
+                <div className="typefaces_footer">
+                    <div className="typefaces_fonts">
+                        <h4>Typefaces</h4>
+                        <ul className="list_items_ul">
+                            <li><a className="list_items" href="">Items</a></li>
+                            <li><a className="list_items" href="">Lateral</a></li>
+                            <li><a className="list_items" href="">Apris</a></li>
+                            <li><a className="list_items" href="">Scto Grotesk</a></li>
+                            <li><a className="list_items" href="">Noe</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="custom_footer">
+                    <div className="custom_items">
+                        <h4>Custom</h4>
+                        <ul className="list_items_ul">
+                            <li><a className="list_items" href="">Kinfolk</a></li>
+                            <li><a className="list_items" href="">Marimekko</a></li>
+                            <li><a className="list_items" href="">Moda Operandi</a></li>
+                            <li><a className="list_items" href="">Söderströms</a></li>
+                            <li><a className="list_items" href="">Demi</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="info_footer">
+                    <div className="info_items">
+                        <h4>Information</h4>
+                        <ul className="list_items_ul">
+                            <li><a className="list_items" href="">Studio</a></li>
+                            <li><a className="list_items" href="">Licenses</a></li>
+                            <li><a className="list_items" href="">Disclaimer</a></li>
+                            <li><a className="list_items" href="">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="contact_footer">
+                    <div className="contact__items">
+                        <h4>Get in touch</h4>
+                        <ul className="list_items_ul">
+                            <li><a className="list_items" href="">Email</a></li>
+                            <li><a className="list_items" href="">Facebook</a></li>
+                            <li><a className="list_items" href="">Instagram</a></li>
+                            <li><a className="list_items" href="">Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
+    );
+}
+
+
+export default MiFooter;
