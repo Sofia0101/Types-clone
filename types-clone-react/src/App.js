3b3913c250b3ce4e5281b867884bfa277b00ef3e
Auca Maillot <auca.maillot@farmacity.com.ar>
2023-10-13 17:34:01 -0300

    limpio body

--- types-clone-react/src/App.js
+++ types-clone-react/src/App.js
@@ -1,33 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import MiHeader from './components/header';
-import "./css/header.css"
-import MiFooter from './components/footer';
-import "./css/footer.css"
-import MiSection from './components/section1';
-import "./css/container.css"
-import "./css/types.css"
-
+import logo from "./logo.svg";
+import "./App.css";
+import MiHeader from "./components/header";
+import "./css/header.css";
+import MiFooter from "./components/footer";
+import "./css/footer.css";
+import MiSection from "./components/section1";
+import "./css/container.css";
+import "./css/types.css";
 
 function App() {
   return (
     <>
-      <body>
-
+      
         <MiHeader />
-        
+
         <main>
-
           <MiSection />
-
         </main>
 
         <MiFooter />
-
-      </body >
+      
     </>
-
-
   );
 }
 
